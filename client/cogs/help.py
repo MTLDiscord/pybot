ccd46069d5b4a7a20d696c3f693aa0eef3fb6c7a
/mtl <102648531+MTLDiscord@users.noreply.github.com>
2022-04-28 10:32:51 +0300

    Update help.py

--- client/cogs/help.py
+++ client/cogs/help.py
@@ -16,24 +16,17 @@ class HelpCommand(commands.Cog):
 
     async def handleHelpCommand(self, ctx):
         embed = discord.Embed()
-        embed.colour = discord.Colour.blurple()
+        embed.colour = 0x7289DA
         embed.set_author(name=f'Welcome to {self.bot.user.name}', icon_url=self.bot.user.display_avatar.url)
-        embed.description = f"{self.bot.user.name} is an open source bot specialising in reports!\n\n" \
-                            "For a detailed list of commands, features and examples, visit my [Top.gg](https://top.gg/bot/950765718209720360) page!"
-        embed.add_field(name="__Commands__", value=
-        f'**/help** - Displays help menu.\n'
-        f'**Report Message** - Right click a message, scroll to \'Apps\', then click me to report a user.\n'
-        f'**Report User** - Right click a user, scroll to \'Apps\', then click me to report a message.\n'
-        f'**/report** - Used to report a user, as mobile devices do not support context menus.\n'
-        f'**/settings** - Used to setup the bot in your server.\n'
-        f'**/about** - Some stats about the bot.\n'
-        f'**/vote** - Shows your voting history.', inline=False)
+        embed.description = f"{self.bot.user.name} - бот с открытым исходным кодом.\n\n" \
+                            "Для детальной информации о командах, посетите мой [GitHub](https://github.com/MTLDiscord/pybot)."
+        embed.add_field(name="__Команды__", value=
+        f'**/help** - Помощь по командам.\n'
+        f'**/help** - Проверка задержки API.\n', inline=False)
 
         view = discord.ui.View()
-        view.add_item(discord.ui.Button(label="Top.gg", emoji="<:topgg:963166927843364874>", url="https://top.gg/bot/950765718209720360"))
-        view.add_item(discord.ui.Button(label="GitHub", emoji="<:github:962089212365111327>", url="https://github.com/Co0kei/Coffee-Bot"))
+        view.add_item(discord.ui.Button(label="GitHub", emoji="<:github:962089212365111327>", url="https://github.com/MTLDiscord/pybot"))
         view.add_item(discord.ui.Button(label="Invite", emoji="📩", url=discord.utils.oauth_url(self.bot.user.id, permissions=discord.Permissions(8))))
-
         await ctx.send(embed=embed, view=view, ephemeral=False)
 
 
